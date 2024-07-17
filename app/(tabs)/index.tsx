44f0adb28b8545bc769ed1b1120e17d0a3433de7
Sara Ahmed <sara.ahmed1447@gmail.com>
2024-07-17 16:56:43 -0700

    login-screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,163 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from "react-native";
 
 export default function HomeScreen() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  const handleLogin = () => {
+    Alert.alert(
+      "Login Button Pressed",
+      `Username: ${username}, Password: ${password}`
+    );
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/images/vistacan-logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>VISTACAN EMR</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[
+            styles.selectMenu,
+            selectedValue === "seemymd" && styles.selectedMenu,
+          ]}
+          onPress={() => setSelectedValue("seemymd")}
+        >
+          <Text
+            style={[
+              styles.selectMenuText,
+              selectedValue === "seemymd" && styles.selectedMenuText,
+            ]}
+          >
+            seemymd
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.selectMenu,
+            selectedValue === "bookmymd" && styles.selectedMenu,
+          ]}
+          onPress={() => setSelectedValue("bookmymd")}
+        >
+          <Text style={[
+              styles.selectMenuText,
+              selectedValue === "bookmymd" && styles.selectedMenuText,
+            ]}>bookmymd</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.selectMenu,
+            selectedValue === "vemr" && styles.selectedMenu,
+          ]}
+          onPress={() => setSelectedValue("vemr")}
+        >
+          <Text style={[styles.selectMenuText, selectedValue === 'vemr' && styles.selectedMenuText]}>vemr</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    padding: 20,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  logo: {
+    marginBottom: 50,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  dropdown: {
+    width: "100%",
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 10,
+    borderRadius: 5,
+    width: "100%",
+  },
+  selectMenu: {
+    width: 100,
+    backgroundColor: "#f3f3f3",
+    borderRadius: 5,
+    paddingVertical: 10,
+  },
+  selectedMenu: {
+    width: 100,
+    backgroundColor: "#007bff",
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  selectedMenuText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  selectMenuText: {
+    color: "#000",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
